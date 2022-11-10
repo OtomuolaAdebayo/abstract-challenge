9abed557ea33fca93a812c7f78bb889430ab7b9b
TheNewWebGuy <114394143+TheNewWebGuy@users.noreply.github.com>
2022-11-10 07:40:59 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const popUpSignIn = function () {
+    document.querySelector(".form-popup").style.display = "block";
+}
+const signIn = document.querySelector(".btn-primary");
+signIn.addEventListener("click", popUpSignIn);
+
+const closeForm = function () {
+    document.querySelector(".form-popup").style.display = "none";
+}
+
+window.onclick = function (event) {
+  let closed = document.querySelector(".form-popup");;
+  if (event.target == closed) {
+    closeForm();
+  }
+}
